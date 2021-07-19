cb28a551479b4daf7637a2db8e7594e864622119
Oliver Sandoval <sandovalot@hotmail.com>
2021-07-19 12:12:22 -0500

    video 106 Pytest

--- tests/test_user.py
+++ tests/test_user.py
@@ -2,36 +2,38 @@ import pytest
 
 from apps.usuario.models import Usuario
 
-@pytest.mark.django_db
-def test_common_user_creation():
-    user = Usuario.objects.create_user(
-            username='dasdas',
-            email='asdasda@gmail.com',
-            nombres='wegw gwgwe',
-            password='12345',
-            is_staff=False
-    )
-    assert user.username == 'dasdas'
+@pytest.fixture
+def user_creation():
+    return Usuario(
+                username='kjbkwejfw',
+                email='asdasd@gmail.com',
+                nombres='sadasd fasfas',
+                password='12345'
+            )
 
 @pytest.mark.django_db
-def test_superuser_creation():
-    user = Usuario.objects.create_superuser(
-            username='dasdas',
-            email='asdasda@gmail.com',
-            nombres='wegw gwgwe',
-            password='12345'
-    )
-
-    assert user.is_superuser
+def test_common_user_creation(user_creation):
+    user_creation.is_staff = False
+    user_creation.save()
+    assert user_creation.is_staff == False
 
 @pytest.mark.django_db
-def test_staff_user_creation():
-    user = Usuario.objects.create_user(
-            username='dasdas',
-            email='asdasda@gmail.com',
-            nombres='wegw gwgwe',
-            password='12345',
-            is_staff=False
-    )
+def test_superuser_creation(user_creation):
+    user_creation.is_superuser = True
+    user_creation.is_staff = True
+    user_creation.save()
+    assert user_creation.is_superuser
 
-    assert user.is_staff
+@pytest.mark.django_db
+def test_staff_user_creation(user_creation):
+    user_creation.is_staff = True
+
+    assert user_creation.is_staff
+
+@pytest.mark.django_db
+def test_user_creation_fail():
+    with pytest.raises(Exception):  
+        Usuario.objects.create_user(
+                password='12345',
+                is_staff=False
+        )
