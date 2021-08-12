d1c669cf8c577e809ada31088f4d2dce0b5f128a
Oliver Sandoval <sandovalot@hotmail.com>
2021-08-12 12:39:24 -0500

    explicacion de uso de clase TestCase de Django

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,7 +1,11 @@
 import pytest
 
-from apps.usuario.models import Usuario
+from django.test import TestCase
 
+from apps.usuario.models import Usuario
+from tests.factories import UsuarioAdminFactory, UsuarioComunFactory
+
+"""
 @pytest.mark.django_db
 def test_common_user_creation(user_creation):
     print(user_creation.rol)
@@ -25,3 +29,19 @@ def test_user_creation_fail():
                 password='12345',
                 is_staff=False
         )
+"""
+
+class UsuarioTestCase(TestCase):
+
+    def setUp(self):
+        self.common_user = UsuarioComunFactory.create()
+        self.superuser = UsuarioAdminFactory.create()
+    
+    def test_common_user_creation(self):
+        self.assertEqual(self.common_user.is_active, True)
+        self.assertEqual(self.common_user.is_staff, False)
+        self.assertEqual(self.common_user.is_superuser, False)
+    
+    def test_suerpuser_creation(self):
+        self.assertEqual(self.superuser.is_staff, True)
+        self.assertEqual(self.superuser.is_superuser, True)
